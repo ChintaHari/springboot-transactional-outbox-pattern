6f47b63be20b5bbbb18ac154e1d9c48364215e43
Hari Krishna Chinta <hkharikrishna62@gmail.com>
2024-08-08 18:42:23 -0500

    Order-service and order-poller

---
+++ order-poller/src/main/java/com/example/order_poller/repository/OutboxRepository.java
@@ -0,0 +1,14 @@
+package com.example.order_poller.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.order_poller.entity.Outbox;
+
+@Repository
+public interface OutboxRepository extends JpaRepository<Outbox, Long> {
+    
+    List<Outbox> findByIsProcessedFalse();
+}
